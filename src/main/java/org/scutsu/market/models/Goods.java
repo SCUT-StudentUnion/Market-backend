9e36563dfc40c357f019ce4b6e0218f713159a7a
胡玮文 <huww98@outlook.com>
2018-10-19 01:32:01 +0800

    主要优化了goods goods不会显示为goodses了 新增商品状态 新增商品创建时间和上架时间 /goods接口现在会同时返回分类和图片信息了 修复一个表名错误

--- src/main/java/org/scutsu/market/models/Goods.java
+++ src/main/java/org/scutsu/market/models/Goods.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +36,15 @@ public class Goods {
 	@Column(precision = 12, scale = 2)
 	private BigDecimal sellingPrice;
 
-	@ManyToOne
+	@ManyToOne(optional = false)
 	private User releasedBy;
+
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private GoodsStatus status;
+
+	@NotNull
+	private OffsetDateTime createdTime;
+
+	private OffsetDateTime onShelfTime;
 }
--- src/main/java/org/scutsu/market/repositories/GoodsRepository.java
+++ src/main/java/org/scutsu/market/repositories/GoodsRepository.java
@@ -1,8 +1,11 @@
 package org.scutsu.market.repositories;
 
 import org.scutsu.market.models.Goods;
+import org.scutsu.market.models.GoodsInlineCategory;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(excerptProjection = GoodsInlineCategory.class, path = "goods", collectionResourceRel = "goods")
 public interface GoodsRepository extends CrudRepository<Goods, Long> {
 
 }
