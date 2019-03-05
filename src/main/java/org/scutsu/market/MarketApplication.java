359ed84dbf044d5b77ddcab2d860b0c24a6a8a96
胡玮文 <huww98@outlook.com>
2019-03-05 22:22:46 +0800

    Hard code China locale and time zone.

--- src/main/java/org/scutsu/market/MarketApplication.java
+++ src/main/java/org/scutsu/market/MarketApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.Clock;
+import java.util.Locale;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class MarketApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+		Locale.setDefault(Locale.CHINA);
+
 		SpringApplication.run(MarketApplication.class, args);
 	}
 
